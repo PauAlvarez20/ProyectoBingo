6c818bc384ab9ae6f3ba01d73413794da207cabc
CamilaCC <cacoreaco@est.utn.ac.cr>
2025-11-20 00:24:30 -0600

    Paquete mainBingo
    MainBingo de la interfaz

---
+++ src/mainBingo/MainBingo.java
@@ -0,0 +1,22 @@
+package mainBingo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import GUI.frmPrincipal;
+/**
+ *
+ * @author ccore
+ */
+public class MainBingo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new frmPrincipal().setVisible(true);  
+    }
+    
+}
